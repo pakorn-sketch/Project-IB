8b05fd96deb5bb28ca9f9cdb27ebd775eed387b6
pakorn-sketch <khunakorn.r@mrdiy.com>
2026-07-03 22:28:02 +0700

    Connect Google Sheet

--- api/sheet.js
+++ api/sheet.js
@@ -1,2 +1,12 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbyYNIGkhzTs-ZtQ4zgV3evYdVjmgh49a74Lze0YZzy66uyVqFmLbhFNxZTW10oPBvs/exec";
+
+async function getData(){
+
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+    return data;
+
+}
--- js/app.js
+++ js/app.js
@@ -1,1 +1,81 @@
-console.log("IB Pending Monitor Started");
+window.onload=()=>{
+
+    loadDashboard();
+
+}
+
+async function loadDashboard(){
+
+    try{
+
+        const data=await getData();
+
+        console.log(data);
+
+        document.getElementById("lastUpdate").innerHTML=
+        "Last Update : "+new Date().toLocaleString();
+
+        createSummary(data);
+
+    }
+
+    catch(e){
+
+        console.log(e);
+
+    }
+
+}function createSummary(data){
+
+    document.getElementById("pendingIB").innerHTML=data.length;
+
+    let sku=0;
+
+    let cost=0;
+
+    let aging=0;
+
+    let warning=0;
+
+    let critical=0;
+
+    data.forEach(x=>{
+
+        sku+=Number(x["SKU Pending"]||0);
+
+        cost+=Number(x["Cost IB"]||0);
+
+        aging+=Number(x["Aging"]||0);
+
+        if(Number(x["Aging"])>=42)
+            warning++;
+
+        if(Number(x["Aging"])>=56)
+            critical++;
+
+    });
+
+    document.getElementById("pendingSKU").innerHTML=
+    sku.toLocaleString();
+
+    document.getElementById("pendingCost").innerHTML=
+    cost.toLocaleString(undefined,{
+        maximumFractionDigits:0
+    });
+
+    document.getElementById("avgAging").innerHTML=
+    (aging/data.length).toFixed(1);
+
+    document.getElementById("warning").innerHTML=
+    warning;
+
+    document.getElementById("critical").innerHTML=
+    critical;
+
+}document
+.getElementById("refreshBtn")
+.onclick=()=>{
+
+    loadDashboard();
+
+}
